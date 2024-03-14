257b76c7689cc08bfc4ab3ad9c2973bf64698055
Luca Severo <luca98.severo@gmail.com>
2024-03-14 00:19:58 -0300

    Rudimentar seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if Rails.env.development?
+  Board.destroy_all
+  User.destroy_all
+end
+
+luca = User.create!(
+  username: "lucasevero",
+  email: "luca@severo.com",
+  password: "123456"
+)
+
+cleo = User.create!(
+  username: "cleosevero",
+  email: "cleo@severo.com",
+  password: "123456"
+)
+
+Board.new(
+  white: luca,
+  black: cleo
+)
